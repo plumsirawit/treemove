985a6b28731653770d68f64e47d26c71fe649810
Sirawit Pongnakintr <sirawit.pongnakintr@polytechnique.edu>
2023-11-04 18:00:26 +0100

    chore: level names

--- LevelSeq.hs
+++ LevelSeq.hs
@@ -10,26 +10,26 @@ import Levels.L4 (t)
 
 loadLevel :: String -> Maybe LTree
 loadLevel id = case id of
-  "L1" -> Just Levels.L1.t
-  "L2" -> Just Levels.L2.t
-  "L3" -> Just Levels.L3.t
-  "L4" -> Just Levels.L4.t
+  "L1-hello" -> Just Levels.L1.t
+  "L2-happy" -> Just Levels.L2.t
+  "L3-water" -> Just Levels.L3.t
+  "L4-haste" -> Just Levels.L4.t
   _ -> Nothing
 
 nextLevelIdent :: GameState -> Maybe String
 nextLevelIdent z = case levelIdent z of
-  "L1" -> Just "L2"
-  "L2" -> Just "L3"
-  "L3" -> Just "L4"
-  "L4" -> Nothing
+  "L1-hello" -> Just "L2-happy"
+  "L2-happy" -> Just "L3-water"
+  "L3-water" -> Just "L4-haste"
+  "L4-haste" -> Nothing
   _ -> Nothing
 
 firstLevel :: GameState
 firstLevel =
   GameState
-    { levelIdent = "L1",
-      initialLevel = fromJust $ loadLevel "L1",
-      zipper = fromJust $ zipToStart $ fromJust $ loadLevel "L1",
+    { levelIdent = "L1-hello",
+      initialLevel = fromJust $ loadLevel "L1-hello",
+      zipper = fromJust $ zipToStart $ fromJust $ loadLevel "L1-hello",
       movesCount = 0,
       bonusCount = 0,
       inventory = Nothing
